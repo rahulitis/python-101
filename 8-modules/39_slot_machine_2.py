58114afd1f6e5c2b71a9266b594179ac973c19e0
rahul <76214053+rahulitis@users.noreply.github.com>
2024-07-29 17:24:08 +0530

    Update 39_slot_machine_2.py

--- 8-modules/39_slot_machine_2.py
+++ 8-modules/39_slot_machine_2.py
@@ -1,31 +1,19 @@
 # Slot Machine 🎰
 # Codédex
-
 import random
 
-symbols = [
-  '🍒',
-  '🍇',
-  '🍉',
-  '7️⃣'
-]
-
 def play():
+    symbols = ['🍒', ' 🍇', '🍉', '7️⃣']
+    while True:
+        results = random.choices(symbols, k=3)
+        print(f'{results[0]} | {results[1]} | {results[2]}')
+        
+        if results[0] == results[1] == results[2]:
+            print("YOU WIN A JACKPOT BABY, ENJOY!")
+        else:
+            play_again = input("Do you want to play again? (Y/N): ")
+            if play_again.upper() == 'N':
+                print("shakalaka")
+                break
 
-  for i in range(1, 51):    
-    results = random.choices(symbols, k=3)
-    print(f'{results[0]} | {results[1]} | {results[2]}')
-    win = results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣'
-
-    if win:
-      print('Jackpot!!! 💰')
-      break
-    else:
-      results = random.choices(symbols, k=3)
-
-answer = ''
-while answer.upper() != 'N':
-  play()
-  answer = input('Keep playing? (Y/N) ')
-
-print('Thanks for playing!')
+play()
